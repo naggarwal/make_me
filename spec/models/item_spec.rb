a7c0f158ef1aecc2456f51a7b0838ec74e4bedfd
Adarsh Pandit <adarsh@thoughtbot.com>
2013-03-28 18:45:49 -0700

    Add tests for validations and associations

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Item, 'associations' do
+  it { expect(subject).to belong_to(:list) }
+end
+
+describe Item, 'validations' do
+  it { expect(subject).to validate_presence_of(:name) }
+  it { expect(subject).to ensure_length_of(:name).is_at_least(3) }
+  it { expect(subject).to ensure_length_of(:name).is_at_most(30) }
+
+  it { expect(subject).to ensure_length_of(:short_description).
+       is_at_most(140) }
+end
