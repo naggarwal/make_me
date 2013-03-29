738443a115d83d46dbebef024e80eb985e04a84a
Adarsh Pandit <adarsh@thoughtbot.com>
2013-03-28 18:43:36 -0700

    Reformat attrs and validations
    * Remove commented scope
    * Remove whitespace and extra newlines

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,21 @@
 class Item < ActiveRecord::Base
-  attr_accessible :name, :source, :selected, :number_of_skips, :short_description, :image_url, :main_url
+  attr_accessible :image_url,
+    :main_url,
+    :name,
+    :number_of_skips,
+    :selected,
+    :short_description,
+    :source
+
   belongs_to :list
 
+  validates :name, presence: true
+  validates :name, length: { within: 3..30 }
+  validates :short_description, length: { maximum: 140 }
+
   scope :get_selected, lambda{|id| where({ :selected => true, :list_id => "#{id}" }) }
- # scope :get_working_set, lambda{|id| where({:selected => false, :list_id => "#{id}", :completed_on => nil})  }
+
   scope :get_working_set, lambda{|id| where(["selected = false AND list_id = ? AND completed_on IS NULL and number_of_skips <= ?", id, 3])  }
-  validates :name,
-  :presence => true, :length => {:within => 3..30}
-  validates :short_description,
-  :length => {:maximum => 140}
 
   def self.select_the_next_one(list_id)
     not_selected_list = Item.get_working_set(list_id)
@@ -17,10 +24,8 @@ class Item < ActiveRecord::Base
       working.selected = true
       working.save
       return working
-    else 
+    else
       return nil
     end
-
   end
-
 end
